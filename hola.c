8db8a4122b46d40282e271ecd87e6ed797d1a0f1
Jeran0724 <jeranminaya@gmail.com>
2026-02-05 17:26:51 -0400

    Updated

--- hola.c
+++ hola.c
@@ -1,6 +1,10 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <stdio.h>
 
-int main (
-    printf("Hola");
-    return 0 ;
-);
+void app_main(void) {
+  while (1) {
+    printf("Holaaa Mundooooo\n");
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
